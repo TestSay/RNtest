819f1c30c294d644d952aea957f1ceac3dd2c47d
Nubdubi <47843014+Nubdubi@users.noreply.github.com>
2021-06-23 11:59:53 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,13 +5,13 @@ import 'firebase/storage'
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAWCga2bTPQNF5joxSz4GT-OZmMurOROSA",
-  authDomain: "rn-test-2ba8d.firebaseapp.com",
-  projectId: "rn-test-2ba8d",
-  storageBucket: "rn-test-2ba8d.appspot.com",
-  messagingSenderId: "109068314071",
-  appId: "1:109068314071:web:8d4c0a08e7ec41553f5792",
-  measurementId: "G-J94JWNW5FL"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 let app;
